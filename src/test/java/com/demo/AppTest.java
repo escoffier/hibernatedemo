7b0c28e134642709b76f8a122fa409304ea05a5c
robbie <qunfengqiu@gmail.com>
2019-04-26 20:29:50 +0800

    commit

--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -2,9 +2,9 @@ package com.demo;
 
 import static org.junit.Assert.assertTrue;
 
-import com.demo.models.Quote;
-import com.demo.models.Stock;
-import com.demo.models.StockExchange;
+//import com.demo.models.Quote;
+//import com.demo.models.Stock;
+//import com.demo.models.StockExchange;
 import junit.framework.TestCase;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -38,43 +38,43 @@ import java.util.List;
 //    }
 //}
 
-public class AppTest extends TestCase {
-    private SessionFactory sessionFactory;
-
-    @Override
-    protected void setUp() throws Exception {
-        //super.setUp();
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
-        try {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        } catch (Exception e) {
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        //super.tearDown();
-        if (sessionFactory != null) {
-            sessionFactory.close();
-        }
-    }
-
-    public void testBasicUsage() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Stock stock = new Stock("600666", StockExchange.shanghai);
-        Quote quote = new Quote(stock, "99.99");
-        session.save(quote );
-        session.getTransaction().commit();
-        session.close();
-
-        session = sessionFactory.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Quote").list();
-        for ( Quote quote1 : (List<Quote>) result ) {
-            System.out.println( quote1.toString() );
-        }
-
-    }
-}
+//public class AppTest extends TestCase {
+//    private SessionFactory sessionFactory;
+//
+//    @Override
+//    protected void setUp() throws Exception {
+//        //super.setUp();
+//        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+//        try {
+//            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+//        } catch (Exception e) {
+//            StandardServiceRegistryBuilder.destroy(registry);
+//        }
+//    }
+//
+//    @Override
+//    protected void tearDown() throws Exception {
+//        //super.tearDown();
+//        if (sessionFactory != null) {
+//            sessionFactory.close();
+//        }
+//    }
+//
+//    public void testBasicUsage() {
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        Stock stock = new Stock("600666", StockExchange.shanghai);
+//        Quote quote = new Quote(stock, "99.99");
+//        session.save(quote );
+//        session.getTransaction().commit();
+//        session.close();
+//
+//        session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        List result = session.createQuery("from Quote").list();
+//        for ( Quote quote1 : (List<Quote>) result ) {
+//            System.out.println( quote1.toString() );
+//        }
+//
+//    }
+//}
